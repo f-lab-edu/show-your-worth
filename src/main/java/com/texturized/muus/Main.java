91675abfdb42aa0e3a0b5cde604115882fea47d8
Jisu Choi <redgem92@gmail.com>
2023-06-28 14:40:07 +0900

    CHORE : application.yaml 설정
    - 프로파일 세분화(local, dev, prod)
    - local 빌드 설정

--- src/main/java/com/texturized/muus/Main.java
+++ src/main/java/com/texturized/muus/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MuusApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MuusApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
