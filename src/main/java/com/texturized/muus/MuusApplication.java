0d0892e58b7cede1f12eb30427a2b2545cc2b7bf
Jisu Choi <redgem92@gmail.com>
2023-06-28 11:50:01 +0900

    Initial Commit - Project

---
+++ src/main/java/com/texturized/muus/MuusApplication.java
@@ -0,0 +1,13 @@
+package com.texturized.muus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/texturized/muus/MuusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texturized.muus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
