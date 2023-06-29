7b0363b97bc99d9bc74ed38d78482f9540b175e3
Jisu Choi <redgem92@gmail.com>
2023-06-29 17:56:52 +0900

    CHORE : 로컬 프로파일 h2 연동

--- src/main/java/com/texturized/muus/Main.java
+++ src/main/java/com/texturized/muus/Main.java
@@ -1,31 +1,13 @@
 package com.texturized.muus;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-import javax.annotation.PostConstruct;
-
-//@SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class Main {
-
-	@Value("${common}")
-	private String common;
-
-	@Value("${test}")
-	private String test;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
-	@PostConstruct
-	private void start() {
-		System.out.println("common = " + common);
-		System.out.println("test = " + test);
-	}
 }
