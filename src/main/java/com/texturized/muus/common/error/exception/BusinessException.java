988d0a74d586f7bd8cb47c632d35dd957498026f
Jisu Choi <redgem92@gmail.com>
2023-07-29 10:30:06 +0900

    CHORE : 필드 final 키워드 추가

--- src/main/java/com/texturized/muus/common/error/exception/BusinessException.java
+++ src/main/java/com/texturized/muus/common/error/exception/BusinessException.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class BusinessException extends RuntimeException {
 
-    private ErrorCode errorCode;
+    private final ErrorCode errorCode;
 
     public BusinessException(String message, ErrorCode errorCode) {
         super(message);
