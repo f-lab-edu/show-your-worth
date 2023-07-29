1aa1a00617fb09752bc50e8fed8bbf7c2bdaf9a3
Jisu Choi <redgem92@gmail.com>
2023-07-29 10:30:19 +0900

    CHORE : 코드 리팩토링

--- src/main/java/com/texturized/muus/domain/user/dao/UserFindDao.java
+++ src/main/java/com/texturized/muus/domain/user/dao/UserFindDao.java
@@ -2,7 +2,6 @@ package com.texturized.muus.domain.user.dao;
 
 import com.texturized.muus.domain.user.domain.User;
 import com.texturized.muus.domain.user.exception.UserNotFoundException;
-import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,12 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserFindDao {
 
     private final UserRepository userRepository;
-    private final UserViewMapper userViewMapper;
 
     public Optional<User> findById(final Long id) {
-        final Optional<User> user = userRepository.findById(id);
-        user.orElseThrow(() -> new UserNotFoundException(id));
-        return user;
+        return Optional.of(userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id)));
     }
-
 }
