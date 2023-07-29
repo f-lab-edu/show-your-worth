5d3b1537585ad9623edc0cd98036aec6ec5df500
Jisu Choi <redgem92@gmail.com>
2023-07-29 10:26:24 +0900

    FIX : Method Not Allowed 에러 메시지 수정

--- src/main/java/com/texturized/muus/common/error/ErrorCode.java
+++ src/main/java/com/texturized/muus/common/error/ErrorCode.java
@@ -10,7 +10,7 @@ public enum ErrorCode {
 
     // Common
     INVALID_INPUT_VALUE(400, "C001", " Invalid Input Value"),
-    METHOD_NOT_ALLOWED(405, "C002", " Invalid Input Value"),
+    METHOD_NOT_ALLOWED(405, "C002", " Unsupported HTTP Method"),
     ENTITY_NOT_FOUND(400, "C003", " Entity Not Found"),
     INTERNAL_SERVER_ERROR(500, "C004", " Server Error"),
     INVALID_TYPE_VALUE(400, "C005", " Invalid Type Value"),
