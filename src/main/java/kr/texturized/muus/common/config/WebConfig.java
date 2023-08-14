16cfca05d7ffca02e67eb778f73fe8f50fb05fd3
Jisu Choi <redgem92@gmail.com>
2023-08-15 06:16:54 +0900

    CHORE: Add WebMvcConfigurer
    - It was coded for get messages for validation from properties.
    - But it's hard to get regular expression, so it isn't used.
    - Let's leave this for future

---
+++ src/main/java/kr/texturized/muus/common/config/WebConfig.java
@@ -0,0 +1,28 @@
+package kr.texturized.muus.common.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public MessageSource validationMessageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.addBasenames("classpath:/messages/validation");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(validationMessageSource());
+        return bean;
+    }
+
+}
