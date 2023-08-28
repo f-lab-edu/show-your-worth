1ae665de08751f5da227cf944c70a8e0ce92e429
Jisu Choi <redgem92@gmail.com>
2023-08-28 16:40:28 +0900

    CHORE: Throw test using JUnit module

--- src/test/java/kr/texturized/muus/application/service/calculator/CoordinateCalculatorTest.java
+++ src/test/java/kr/texturized/muus/application/service/calculator/CoordinateCalculatorTest.java
@@ -4,6 +4,7 @@ package kr.texturized.muus.application.service.calculator;
 import static org.assertj.core.api.Assertions.*;
 
 import kr.texturized.muus.application.service.exception.OutOfCoordinateRangeException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CoordinateCalculatorTest {
@@ -24,23 +25,15 @@ class CoordinateCalculatorTest {
 
     @Test
     void invalidRangeThenExpectError() {
-        expectInvalid(0.0, 0.0, 1, 1);
-        expectInvalid(33.0, 124.6, 1, 1);
-        expectInvalid(37.500718, 127.028226, 1_000_000.0, 1_000_000.0);
-    }
-
-    void expectInvalid(double latitude, double longitude, double widthMeter, double heightMeter) {
-        try {
-            calculator.validateRange(latitude, longitude, widthMeter, heightMeter);
-            fail(String.format(
-                "Expected error but it is valid: l: %f, r: %f, w: %f, h: %f%n",
-                latitude,
-                longitude,
-                widthMeter,
-                heightMeter
-            ));
-        } catch(OutOfCoordinateRangeException e) {
-        }
+        Assertions.assertThrows(OutOfCoordinateRangeException.class, () ->{
+            calculator.validateRange(0.0, 0.0, 1, 1);
+        });
+        Assertions.assertThrows(OutOfCoordinateRangeException.class, () ->{
+            calculator.validateRange(33.0, 124.6, 1, 1);
+        });
+        Assertions.assertThrows(OutOfCoordinateRangeException.class, () ->{
+            calculator.validateRange(37.500718, 127.028226, 1_000_000.0, 1_000_000.0);
+        });
     }
 
     @Test
