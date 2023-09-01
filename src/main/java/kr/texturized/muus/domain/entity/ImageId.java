6788500f38764762bbc4f3a46f4f3eb3cca7e3a2
Jisu Choi <redgem92@gmail.com>
2023-09-01 15:07:22 +0900

    FEAT: Entity for Image

---
+++ src/main/java/kr/texturized/muus/domain/entity/ImageId.java
@@ -0,0 +1,59 @@
+package kr.texturized.muus.domain.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Composite-id class for keyword.
+ */
+@Getter
+public class ImageId implements Serializable {
+
+    @NotBlank
+    @Column(name = "post_id")
+    private final Long postId;
+
+    @NotBlank
+    @Column(name = "category_id")
+    private final PostCategory category;
+
+    @NotBlank
+    @Column(name = "upload_order")
+    private final Integer uploadOrder;
+
+    @Builder
+    public ImageId(
+        final Long postId,
+        final PostCategory category,
+        final Integer uploadOrder) {
+        this.postId = postId;
+        this.category = category;
+        this.uploadOrder = uploadOrder;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (o instanceof ImageId id) {
+            return (this.postId.compareTo(id.postId) == 0
+                && this.category.compareTo(id.category) == 0
+                && this.uploadOrder.compareTo(id.uploadOrder) == 0);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(postId, category, uploadOrder);
+    }
+}
