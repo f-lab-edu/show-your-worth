36aae77acb42d6b39c28600fd5da8795a06c3703
Jisu Choi <redgem92@gmail.com>
2023-11-20 20:41:10 +0900

    REFACTOR: Redis Session Container in local

--- src/main/java/kr/texturized/muus/common/config/LocalRedisConfig.java
+++ src/main/java/kr/texturized/muus/common/config/LocalRedisConfig.java
@@ -6,16 +6,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @Profile("local")
 @Configuration
-public class RedisConfig {
+public class LocalRedisConfig {
 
     @Value("${spring.redis.session.host}")
     private String redisSessionHost;
 
     @Value("${spring.redis.session.port}")
     private int redisSessionPort;
+
+    private static final String REDIS_IMAGE = "redis:7.2.2-alpine3.18";
+
+    private static final GenericContainer<?> REDIS_CONTAINER;
+
+    static {
+        REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse(REDIS_IMAGE))
+            .withExposedPorts(6379)
+            .withReuse(true);
+
+        REDIS_CONTAINER.start();
+
+        System.setProperty("spring.redis.session.host", REDIS_CONTAINER.getHost());
+        System.setProperty("spring.redis.session.port", REDIS_CONTAINER.getMappedPort(6379).toString());
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
