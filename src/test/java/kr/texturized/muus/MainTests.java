2006fa0b8e8312defe0e06391134d71536c75399
Jisu Choi <redgem92@gmail.com>
2023-11-21 15:05:07 +0900

    CHORE: Test for failure

--- src/test/java/kr/texturized/muus/MainTests.java
+++ src/test/java/kr/texturized/muus/MainTests.java
@@ -1,11 +1,13 @@
 package kr.texturized.muus;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MainTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail();
 	}
 
 }
