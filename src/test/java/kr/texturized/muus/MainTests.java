5f1a62bafd2f11cea9f0280be13dd5e1351adb57
Jisu Choi <redgem92@gmail.com>
2023-11-21 16:18:25 +0900

    REFACTOR: Move mapper to simpler package

--- src/test/java/kr/texturized/muus/MainTests.java
+++ src/test/java/kr/texturized/muus/MainTests.java
@@ -1,13 +1,11 @@
 package kr.texturized.muus;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MainTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.fail();
 	}
 
 }
